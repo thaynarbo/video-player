b571ae837789f87217c34045c3edca71297e3ea1
thaynarbo <thaynarbo@gmail.com>
2022-04-07 22:10:54 -0300

    add icon and progress lib

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
 import React from 'react'
+import ProgressBar from 'react-native-progress/Bar'
 import Video from 'react-native-video';
+import Icon from 'react-native-vector-icons/AntDesign';
+
 export  function VideoPlayer() {
   return (
     <View style={styles.container}>
         <View style={styles.videoContainer}>
             <Video source={{uri:'https://www.w3schools.com/html/mov_bbb.mp4'}} style={styles.video} resizeMode={'cover'} repeat/>   
+       <View style={styles.controls}>
+          <View style={{flex:1,alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
+            <TouchableWithoutFeedback>
+              <Icon name="play" size={18} color="#FFF" />
+            </TouchableWithoutFeedback>
+          <ProgressBar progress={0.4} color="#FFF" width={210} />
+          </View>
+       </View>
         </View>
     </View>
   )
@@ -26,9 +37,18 @@ const styles = StyleSheet.create({
   alignItems:'center',
   overflow:'hidden'
 },
-  video:{
+video:{
   width:'100%',
   height:'100%',
-  position:'absolute'
+ 
 },
+controls:{
+  position:'absolute',
+  backgroundColor:'rgba(0,0,0,0.5)',
+  width:'100%',
+  height: 30,
+  bottom: 0,
+  justifyContent:'center',
+  alignItem:'center'
+}
 })
