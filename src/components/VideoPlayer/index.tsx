7adc88305dab58cf62ef5f6adc1ed7e1cee9e306
thaynarbo <thaynarbo@gmail.com>
2022-04-10 10:43:39 -0300

    formatting and reading react-native-video lib

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,54 +1,66 @@
-import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import React, { useRef, useState } from 'react'
 import ProgressBar from 'react-native-progress/Bar'
 import Video from 'react-native-video';
 import Icon from 'react-native-vector-icons/AntDesign';
 
-export  function VideoPlayer() {
+
+export function VideoPlayer() {
+  const ref = useRef(null)
+  const [paused, setPaused] = useState(true)
+  const [currentTime, setCurrentTime] = useState(0)
+  const renderIcon = paused ? 'play' : 'pause'
+
   return (
     <View style={styles.container}>
-        <View style={styles.videoContainer}>
-            <Video source={{uri:'https://www.w3schools.com/html/mov_bbb.mp4'}} style={styles.video} resizeMode={'cover'} repeat/>   
-       <View style={styles.controls}>
-          <View style={{flex:1,alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
-            <TouchableWithoutFeedback>
-              <Icon name="play" size={18} color="#FFF" />
-            </TouchableWithoutFeedback>
-          <ProgressBar progress={0.4} color="#FFF" width={210} />
+      <View style={styles.videoContainer}>
+        <Video
+          source={{ uri: 'https://www.w3schools.com/html/mov_bbb.mp4' }}
+          ref={ref}
+          style={styles.video} resizeMode={'cover'}
+          paused={paused}
+          repeat
+        />
+        <View style={styles.controls}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
+            <TouchableOpacity style={{ marginRight: 15 }} onPress={() => setPaused(!paused)} >
+              <Icon name={renderIcon} size={20} color="#e93515" />
+            </TouchableOpacity>
+            <ProgressBar progress={0.4} color="#FFF" width={210} />
           </View>
-       </View>
         </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+  container: {
+    flex: 1,
+    alignItems: 'center'
+
   },
-  videoContainer:{
-  height:213,
-  width:302,
-  borderRadius:15, 
-  position:'relative',
-  justifyContent:'center',
-  alignItems:'center',
-  overflow:'hidden'
-},
-video:{
-  width:'100%',
-  height:'100%',
- 
-},
-controls:{
-  position:'absolute',
-  backgroundColor:'rgba(0,0,0,0.5)',
-  width:'100%',
-  height: 30,
-  bottom: 0,
-  justifyContent:'center',
-  alignItem:'center'
-}
+  videoContainer: {
+    height: 213,
+    width: 302,
+    borderRadius: 15,
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden'
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+
+  },
+  controls: {
+    position: 'absolute',
+    width: '100%',
+    height: 30,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItem: 'center'
+  }
 })
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import React from 'react'
 import {VideoPlayer} from '../../components/VideoPlayer'
 
 
 export function Home() {
   return (
-    <View style={{flex:1}}>
+    <View style={styles.container}>
+      <TextInput style={styles.inputFilter} />
         <VideoPlayer />
     </View>
    
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1, alignItems:'center',padding:40,backgroundColor:'rgb(26, 17, 41)'
+  },
+  inputFilter:{
+    height:40,width:300,backgroundColor:'rgba(73, 32, 74, 0.35)',borderRadius:10,color:'#fff',
+    marginBottom:30,
+  }
+})
